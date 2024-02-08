4537b188419393f73a55bb697254c663e89455c3
abel <115671826+atomez36@users.noreply.github.com>
2024-02-08 15:26:15 -0600

    <completed javascript status flow>

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,30 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
- 
+ const randomTopic = [Math.floor(Math.random() * topics.length)]; 
+function listTopics()
+{
 for (let x = 0; x <topics.length; x++) {
    console.log(topics[x]);
 }
+ }
+ 
+function helloWorld() {
+  
+}
 
-/*
-if (topic === 'HTML') {
+function SelectTopic() {
+
+if (randomTopics[0] === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic[0] === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic[0] === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic[0] === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
-*/
+
+}
+
+listTopics()
