2c87b12dd156ffd8c13974f5242ff8853bd59f42
abel <115671826+atomez36@users.noreply.github.com>
2024-02-08 15:37:49 -0600

    <edited java workflow>

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,15 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
  const randomTopic = [Math.floor(Math.random() * topics.length)]; 
-function listTopics()
-{
+ 
+ function listTopics() {
 for (let x = 0; x <topics.length; x++) {
    console.log(topics[x]);
-}
  }
- 
-function helloWorld() {
-  
 }
-
+ 
 function SelectTopic() {
 
-if (randomTopics[0] === 'HTML') {
+if (randomTopic[0] === 'HTML') {
   console.log("Let's study HTML!");
 } else if (randomTopic[0] === 'CSS') {
   console.log("Let's study CSS!");
@@ -26,5 +22,7 @@ if (randomTopics[0] === 'HTML') {
 }
 
 }
-
-listTopics()
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('which topic shpould we study first');
+SelectTopic();
